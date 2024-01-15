5ce64e77fb00f3face60d4c51ac009e924783dcf
Zarxrax <zarxrax@yahoo.com>
2024-01-15 12:14:31 -0500

    remove uneeded code and dependencies

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,28 +1,26 @@
 @echo off
-echo Select PyTorch version to install:
+echo Select version to install:
 echo 1. CPU (Select this if you are unsure which option to choose)
-echo 2. CUDA (For NVIDIA GPUs)
-echo 3. Uninstall Pytorch
-echo 4. Exit
+echo 2. CUDA (Faster, only for NVIDIA GPUs)
+echo 3. Exit
 
-choice /C 1234 /N /M "Enter your choice (1, 2, 3, or 4):"
+choice /C 123 /N /M "Enter your choice (1, 2, or 3):"
 
-if errorlevel 4 (
-    echo Exiting without installing PyTorch
+if errorlevel 3 (
+    echo Exiting without installing dependencies
     exit /b 0
-) else if errorlevel 3 (
-    echo Uninstalling existing Pytorch
-    .\python-3.11.7-embed-amd64\python.exe -m pip uninstall torch torchvision
 ) else if errorlevel 2 (
     echo Uninstalling existing Pytorch
     .\python-3.11.7-embed-amd64\python.exe -m pip uninstall torch torchvision
     echo Installing CUDA version of PyTorch
     .\python-3.11.7-embed-amd64\python.exe -m pip install torch torchvision --index-url https://download.pytorch.org/whl/cu118
+	.\python-3.11.7-embed-amd64\python.exe -m pip install -r .\requirements.txt
 ) else (
     echo Uninstalling existing Pytorch
     .\python-3.11.7-embed-amd64\python.exe -m pip uninstall torch torchvision
     echo Installing CPU version of PyTorch
     .\python-3.11.7-embed-amd64\python.exe -m pip install torch torchvision
+	.\python-3.11.7-embed-amd64\python.exe -m pip install -r .\requirements.txt
 )
 
 echo Completed.
