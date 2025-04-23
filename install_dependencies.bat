288b7f417b3f44c2d24bbe297e780491ad69144f
Zarxrax <zarxrax@yahoo.com>
2025-04-23 19:06:22 -0400

    update requirements

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -2,8 +2,8 @@
 pushd %~dp0
 
 echo Select version to install:
-echo 1. CPU (Select this if you are unsure which option to choose)
-echo 2. CUDA (Faster, only for NVIDIA GPUs)
+echo 1. CUDA (Fast processing if you have an NVIDIA GPU)
+echo 2. CPU (Slow performance but smaller download)
 echo 3. Exit
 
 choice /C 123 /N /M "Enter your choice (1, 2, or 3):"
@@ -12,16 +12,18 @@ if errorlevel 3 (
     echo Exiting without installing dependencies
     exit /b 0
 ) else if errorlevel 2 (
-    echo Uninstalling existing Pytorch
-    .\python-3.11.7-embed-amd64\python.exe -m pip uninstall torch torchvision
-    echo Installing CUDA version of PyTorch
-    .\python-3.11.7-embed-amd64\python.exe -m pip install torch==2.5.1 torchvision==0.20.1 --index-url https://download.pytorch.org/whl/cu124
-	.\python-3.11.7-embed-amd64\python.exe -m pip install -r .\requirements.txt
-) else (
+    .\python-3.11.7-embed-amd64\python.exe -m pip install --upgrade pip --no-warn-script-location
     echo Uninstalling existing Pytorch
     .\python-3.11.7-embed-amd64\python.exe -m pip uninstall torch torchvision
     echo Installing CPU version of PyTorch
-    .\python-3.11.7-embed-amd64\python.exe -m pip install torch==2.5.1 torchvision==0.20.1 --index-url https://download.pytorch.org/whl/cpu
+    .\python-3.11.7-embed-amd64\python.exe -m pip install torch==2.7.0 torchvision --index-url https://download.pytorch.org/whl/cpu
+	.\python-3.11.7-embed-amd64\python.exe -m pip install -r .\requirements.txt
+) else (
+    .\python-3.11.7-embed-amd64\python.exe -m pip install --upgrade pip --no-warn-script-location
+    echo Uninstalling existing Pytorch
+    .\python-3.11.7-embed-amd64\python.exe -m pip uninstall torch torchvision
+    echo Installing CUDA version of PyTorch
+    .\python-3.11.7-embed-amd64\python.exe -m pip install torch==2.7.0 torchvision --index-url https://download.pytorch.org/whl/cu128
 	.\python-3.11.7-embed-amd64\python.exe -m pip install -r .\requirements.txt
 )
 
